677cf446b7c918032b6d37a5e788a055a15634f6
Gabor Rapcsanyi <g.rapcsanyi@samsung.com>
2018-05-11 13:26:45 +0100

    Try7

--- sw2.js
+++ sw2.js
@@ -2,6 +2,14 @@ self.addEventListener('fetch', function(event) {
   console.log("Fetching", event.request.url);
   console.log("Request", event.request);
   console.log("Event", event);
-  event.respondWith(fetch(event.request));
-});
+  event.respondWith(
 
+
+    fetch(event.request).then(function(response){
+        console.log('Response', response);
+        return response;
+    })
+
+
+  );
+});
